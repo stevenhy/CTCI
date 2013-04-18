2b694a861d0ca566a476c58ff821522f90739573
stevenhy <haoy@ece.ubc.ca>
2013-04-18 02:07:04 -0700

    8.isRotation

---
+++ chapter1/8.isRotation.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool isSubstring(string s1, string s2) {
+    if(s2.find(s1) != string::npos) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool isRotation(string s1, string s2) {
+    if(s1.size() != s2.size()) {
+        return false;
+    }
+    string s3 = s1 + s1;
+    return isSubstring(s2, s3); 
+}
+
+int main() {
+    string s1 = "waterbottle";
+    string s2 = "erbottlewat";
+    cout << isRotation(s1, s2) << endl;
+    return 0;
+}
