45dc2df28d6a855f8560e41c5e7932761d10882d
stevenhy <haoy@ece.ubc.ca>
2013-05-19 16:01:31 -0700

    add 2.matrix_path

---
+++ chapter9/2.matrix_path.cpp
@@ -0,0 +1,6 @@
+// How many possible paths are there from (0, 0) to (X, Y)
+// If every spot is available.
+// num = number of ways to choose X from X + Y 
+//
+// If certain spots are "off limits"
+// use DP to calculate
